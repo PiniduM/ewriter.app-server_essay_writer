d1e156077bf8595537024fcf0c09812f89f388b7
Pinidu Maneesha <pmjuniorcomtec@gmail.com>
2023-03-03 18:15:26 +0530

    about to push to github

--- src/nonPremiumRoutes/writeAEssay/essayProvider.js
+++ src/nonPremiumRoutes/writeAEssay/essayProvider.js
@@ -27,13 +27,13 @@ const essayProvider = (req, res) => {
   let prompt;
   if (type === "general") {
     const complexity = data.complexity;
-    prompt = `Please write an essay on "${topic}" in ${wordsCount} words.Use ${complexity} vocabulary and sentence structures.`
+    prompt = `You are a Professional essay Writer.Please write an essay on "${topic}" IN ${wordsCount} WORDS.Use ${complexity} vocabulary and sentence structures.`
   }
   else if ((type === "personalized")) {
     const profileData = data.profileData;
     //prompt = `Dear ${profileData.name || "dave"}, as a ${profileData.occupation || "student"} who is ${profileData.age || "18"} years old and from ${profileData.country || "usa"} write a meaningful essay on "${topic}" in ${wordsCount} words.`
     //prompt = `Assume that you are pinidu, a collage student living in sri lanka.add personal references but do not do it unnecessarily.`
-    prompt = `Imagine that you are ${profileData.name || "dave"}, a ${profileData.age || "18"} years old ${profileData.occupation || "student"} from ${profileData.country || "usa"}. Write an essay exploring the topic ${topic} IN ${wordsCount}WORDS. Be sure to incorporate relevant personal details to enhance the essay, but only include information that is necessary to support your arguments and illustrate your perspective.`
+    prompt = `Imagine that you are ${profileData.name || "dave"}, a ${profileData.age || "18"} years old ${profileData.occupation || "student"} from ${profileData.country || "usa"}. Write an essay exploring the topic ${topic} IN ${wordsCount} WORDS. Be sure to incorporate relevant personal details to enhance the essay, but only include information that is necessary to support your arguments and illustrate your perspective.`
     console.log(prompt);
 
   }
