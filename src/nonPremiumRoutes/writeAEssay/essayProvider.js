f016a82597836a5930a6dcabc63ab48577184ee3
Pinidu Maneesha <pmjuniorcomtec@gmail.com>
2023-03-19 10:13:25 +0530

    up

--- src/nonPremiumRoutes/writeAEssay/essayProvider.js
+++ src/nonPremiumRoutes/writeAEssay/essayProvider.js
@@ -1,10 +1,12 @@
 import getCompletion from "../functions/getCompletion.js";
+import axios from "axios";
 
 const essayProvider = (req, res) => {
   const data = req.body;
   const topic = data.topic;
   const wordsCount = data.count;
   const type = data.type;
+
 
   if (!(topic && wordsCount && type)) {
     console.log("details missing");
@@ -18,7 +20,7 @@ const essayProvider = (req, res) => {
     return;
   }
 
-  if (!["70","100","150","200","250"].includes(wordsCount)) {
+  if (!["70", "100", "150", "200", "250"].includes(wordsCount)) {
     console.log("unregistered words count exceeded");
     res.status(406).send("unregisted_words_count_exceeded");
     return;
@@ -27,22 +29,23 @@ const essayProvider = (req, res) => {
   let prompt;
   if (type === "general") {
     const complexity = data.complexity;
-    prompt = `You are a Professional essay Writer.Please write an essay on "${topic}" IN ${wordsCount} WORDS.Use ${complexity} vocabulary and sentence structures.`
-  }
-  else if ((type === "personalized")) {
+    prompt = `imagine that You are a Professional essay Writer.Please write an essay on "${topic}" IN ${wordsCount} WORDS.Use ${complexity} vocabulary and sentence structures.`;
+  } else if (type === "personalized") {
     const profileData = data.profileData;
     //prompt = `Dear ${profileData.name || "dave"}, as a ${profileData.occupation || "student"} who is ${profileData.age || "18"} years old and from ${profileData.country || "usa"} write a meaningful essay on "${topic}" in ${wordsCount} words.`
     //prompt = `Assume that you are pinidu, a collage student living in sri lanka.add personal references but do not do it unnecessarily.`
-    prompt = `Imagine that you are ${profileData.name || "dave"}, a ${profileData.age || "18"} years old ${profileData.occupation || "student"} from ${profileData.country || "usa"}. Write an essay exploring the topic ${topic} IN ${wordsCount} WORDS. Be sure to incorporate relevant personal details to enhance the essay` //,but only include information that is necessary to support your arguments and illustrate your perspective.
-    console.log(prompt);
-
-  }
-  else prompt = `You are a professional essay writer,write a complete and meaningful essay on "${topic}" in ${wordsCount} words.`;
+    prompt = `Imagine that you are ${profileData.fullName || "dave"}, a ${
+      profileData.age || "18"
+    } years old ${profileData.occupation || "student"} from ${
+      profileData.country || "usa"
+    }. Write an essay exploring the topic ${topic} IN ${wordsCount} WORDS. Be sure to incorporate relevant personal details to enhance the essay.`; //,but only include information that is necessary to support your arguments and illustrate your perspective.
+  } else
+    prompt = `You are a professional essay writer,write a complete and meaningful essay on "${topic}" in ${wordsCount} words.`;
 
   //const model = "text-curie-001";
-  prompt += "aware of tenses.do not write anything like 'i am a language model',just write the essay.";
+  prompt +=
+    " aware of tenses.do not write anything like 'i am a language model',just write the essay.";
 
-  console.log(prompt);
 
   getCompletion(prompt)
     .then((completion) => {
