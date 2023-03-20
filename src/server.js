e587d03028febcfbe21b24b407efce4c33f8bcc6
Pinidu Maneesha <pmjuniorcomtec@gmail.com>
2023-03-20 11:14:10 +0530

    up

--- src/server.js
+++ src/server.js
@@ -1,20 +1,24 @@
-import express,{json} from "express";
+import express, { json } from "express";
 import cors from "cors";
-
 
 import nonPremiumRouter from "./nonPremiumRoutes/nonPremiumRouter.js";
 
-
 const server = express();
 
+const corsOptions = {
+  origin: process.env.ALLOWED_ORIGIN,
+  methods: ["POST"],
+};
 
-server.use(cors());
+server.use(cors(corsOptions));
+
+server.use((req, res, next) => {
+  console.log(req.headers);
+  if (req.headers["content-legth"] === "67") next();
+  else res.status(500).send("unauthorized_access");
+});
 server.use(json());
 
-server.use("/nonpremium",nonPremiumRouter);
+server.use("/nonpremium", nonPremiumRouter);
 
-
-
-
-
-server.listen(5003,() => console.log("listening to port:5003"));
+server.listen(5003, () => console.log("listening to port:5003"));
